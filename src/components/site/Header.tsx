785b75df7aca7dbc27c5b09b349374bf6306b0cb
juanjosbg <juanjoseborrero95@gmail.com>
2026-04-22 10:51:27 -0500

    se arreglaron unos cambios para la parte de search Console de las paginas web de las ruta

--- src/components/site/Header.tsx
+++ src/components/site/Header.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import logo from "@/assets/logo-cuidado-express.png";
+import logo from "@/assets/Logo2.png";
 
 const links = [
   { href: "#inicio", label: "Inicio" },
